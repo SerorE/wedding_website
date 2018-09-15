29afad2da592b11bf54cde8c14fb7c72bee56731
Elliot Seror <elliot.seror@edhec.com>
2018-09-15 10:53:00 +0100

    start of fixing js. introduced js controller (in fct on url)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,39 +1,62 @@
 import "bootstrap";
 
+//actions controller here
+
+var url = window.location.href;
+var pwd_array = ["",false];
+
+// if (url == "http://localhost:3000/" ||
+//     url == "http://www.sarahetelliot.fr/" ||
+//     url == "http://sarahetelliot.fr/"){
+// askforcode()
+// }
+
+if (pwd_array[1]==false){
+pwd_array = askforcode()
+sessionStorage.setItem("session_array", pwd_array);
+}
+//changer cette condition à si pas de code valide
+else if (url == "http://localhost:3000/programme" ||
+    url == "http://www.sarahetelliot.fr/programme" ||
+    url == "http://sarahetelliot.fr/programme"){
+pwd_array = sessionStorage.getItem("session_array");
+addToProgramme(pwd_array);
+}
+
+
 
 //only run if homepage
 
-window.onload = function () {
-    // if (localStorage.getItem("hasCodeRunBefore") === null) {
-  jQuery(overlay).fadeIn("slow");
-  jQuery(promptBox).fadeIn("slow");
-    //     localStorage.setItem("hasCodeRunBefore", true);
-    // }
-}
+// window.onload = function () {
+//     // if (localStorage.getItem("hasCodeRunBefore") === null) {
+
+//     //     localStorage.setItem("hasCodeRunBefore", true);
+//     // }
+// }
 
 
-const promptBox = document.getElementById("prompt-box");
-const codeError = document.getElementById("code-error");
-const overlay = document.getElementById("overlay");
-const vendredi = document.getElementById("vendredi");
-console.log(codeError)
-const samedi = document.getElementById("samedi");
+// const promptBox = document.getElementById("prompt-box");
+// const codeError = document.getElementById("code-error");
+// const overlay = document.getElementById("overlay");
+// const vendredi = document.getElementById("vendredi");
+// console.log(codeError)
+// const samedi = document.getElementById("samedi");
 
 
-function activateConsequences(array){
+// function activateConsequences(array){
 
-if (array[1] == true){
-  jQuery(overlay).fadeOut("slow");
-  jQuery(promptBox).fadeOut("slow");
+// if (array[1] == true){
+//   jQuery(overlay).fadeOut("slow");
+//   jQuery(promptBox).fadeOut("slow");
 
-} else {
-    codeError.classList.add("is-error");
-    promptBox.classList.add("shake");
-    setTimeout(function() {
-        promptBox.classList.remove("shake")
-    }, 1000);
-}
-};
+// } else {
+//     codeError.classList.add("is-error");
+//     promptBox.classList.add("shake");
+//     setTimeout(function() {
+//         promptBox.classList.remove("shake")
+//     }, 1000);
+// }
+// };
 
 
 function checkpassword(mdp){
@@ -53,32 +76,33 @@ if (mdp.toLowerCase() == "bary" ||
 return [mdp, solved];
 };
 
-function displayProgramAccordingly(arr){
-if (arr[1] == false){
-  return;
-} else {
-    if (arr[0] == "seror") {
-      console.log("vendredi et dimanche - potes");
-    }
-    else if (arr[0] == "flicoteaux"){
-      console.log("samedi et dimanche - famille");
-    }
-    else if (arr[0] == "bary"){
-      console.log("tout - très proches");
-        vendredi.classList.remove("hide");
-        samedi.classList.remove("hide");
-    }
-    else if (arr[0] == "prawidlo"){
-      console.log("juste dimanche");
-    }
-    else {
-  console.log("erreur, merci de contacter sarah et elliot");
-    };
-}
-};
+// function displayProgramAccordingly(arr){
+// if (arr[1] == false){
+//   return;
+// } else {
+//     if (arr[0] == "seror") {
+//       console.log("vendredi et dimanche - potes");
+//     }
+//     else if (arr[0] == "flicoteaux"){
+//       console.log("samedi et dimanche - famille");
+//     }
+//     else if (arr[0] == "bary"){
+//       console.log("tout - très proches");
+//         vendredi.classList.remove("hide");
+//         samedi.classList.remove("hide");
+//     }
+//     else if (arr[0] == "prawidlo"){
+//       console.log("juste dimanche");
+//     }
+//     else {
+//   console.log("erreur, merci de contacter sarah et elliot");
+//     };
+// }
+// };
 
-
-
+function (askforcode){
+  jQuery(overlay).fadeIn("slow");
+  jQuery(promptBox).fadeIn("slow");
 $('.prompt-box form').submit( function(e) {
 
   e.preventDefault();
@@ -88,7 +112,7 @@ $('.prompt-box form').submit( function(e) {
   displayProgramAccordingly(arr)
 });
 
-
+};
 
 
 
